1a7c98c734aade881dc4dc3b6c2686fded56bc5a
MattRul <mr223mi@student.lnu.se>
2019-12-23 09:43:24 +0100

    Added memory-game bmp, fixed menu

--- src/js/maintemplate.js
+++ src/js/maintemplate.js
@@ -2,7 +2,13 @@ const mainTemplate = document.createElement('template')
 mainTemplate.innerHTML = `
 <link rel="stylesheet" href="css/style.css">
 <div id="mainbox">
+
 <div id="menu">
+
+<div id="memory"></div>
+<div id="chat"></div>
+<div id="otherapp"></div>
+
 </div>
 </div>
 `
