80c2c6ad9efe264e3f401dc81810c467d7fc162c
MattRul <mr223mi@student.lnu.se>
2019-12-23 09:00:49 +0100

    Adding template

--- src/js/maintemplate.js
+++ src/js/maintemplate.js
@@ -1,22 +1,9 @@
 const mainTemplate = document.createElement('template')
 mainTemplate.innerHTML = `
 <link rel="stylesheet" href="css/style.css">
-<div id="header">
-<h1>Memory</h1>
-</div>
 <div id="mainbox">
-<div id="mainheader">
-    <h2>Välj storlek på spelet</h2>
-    <div id="maincontent">
-    </div>
-</div>
-<div id="button">
-<button>2x2</button>
-<button>4x2</button>
-<button>4x4</button>
+<div id="menu">
 </div>
 </div>
-</div>
-<style>#imageBox {width: 200px;}</style>
 `
 export { mainTemplate }
